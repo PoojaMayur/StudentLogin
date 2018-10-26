5f7be9027eae2cb9ffcbe96f97eaef0746406a5d
panduranggiram <girampg0105@gmail.com>
2018-10-26 13:00:49 +0530

    adding maia main in main

--- CRUD/src/main/java/com/main/TestMain.java
+++ CRUD/src/main/java/com/main/TestMain.java
@@ -1,17 +1,19 @@
 
-package com.main
+package com.main;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-@EntityScan(basePackages="com.model")
+@EntityScan(basePackages="com")
 @SpringBootApplication(scanBasePackages="com")
-@EnableJpaRepositories(basePackages="com.DaoInterface")
+@EnableJpaRepositories(basePackages="com")
 
 public class TestMain {
 
 	public static void main(String[] args) {
-		System.out.println("main");
+		System.out.println("main--main");
+		
+		
 		SpringApplication.run(TestMain.class, args);
 		System.out.println("main End");
 	
