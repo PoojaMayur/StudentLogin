0782c2323655e76be9bc106d8e10c1228d56018d
vspande13 <44388494+vspande13@users.noreply.github.com>
2018-10-26 12:17:55 +0530

    Update TestMain.java

--- CRUD/src/main/java/com/main/TestMain.java
+++ CRUD/src/main/java/com/main/TestMain.java
@@ -1,6 +1,5 @@
 
-
-import package com.model
+package com.main
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
