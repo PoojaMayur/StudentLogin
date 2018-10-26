a63b6cfc41e8cf71ec7e664aecc0e5a984815964
vspande13 <44388494+vspande13@users.noreply.github.com>
2018-10-26 11:37:11 +0530

    Update TestMain.java

--- CRUD/src/main/java/com/main/TestMain.java
+++ CRUD/src/main/java/com/main/TestMain.java
@@ -9,7 +9,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 @EnableJpaRepositories(basePackages="com.DaoInterface")
 
 public class TestMain {
-
+mmmmmmmmmmmmmmmmmm
 	public static void main(String[] args) {
 		System.out.println("main");
 		SpringApplication.run(TestMain.class, args);
